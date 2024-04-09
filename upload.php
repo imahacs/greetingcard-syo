e4a65dbcd45edf84a638b6515447a90d6dc7939d
imahacs <mem880.a@gmail.com>
2024-04-10 00:26:37 +0300

    first commit

---
+++ upload.php
@@ -0,0 +1,11 @@
+<?php
+$src = $_FILES["image"]["tmp_name"];
+$imageName = $_FILES["image"]["name"];
+$imageExtension = explode('.', $imageName);
+$imageExtension = strtolower(end($imageExtension));
+$newImageName = uniqid() . "." . $imageExtension;
+$targ = "img/" . $newImageName;
+move_uploaded_file($src, $targ);
+
+echo $targ;
+?>
