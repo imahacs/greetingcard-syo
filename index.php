df1c210cbb08d0439de684fa769d08a978a0350c
imahacs <mem880.a@gmail.com>
2024-04-13 08:11:13 +0300

    Adding files

--- index.php
+++ index.php
@@ -50,7 +50,6 @@
 
   <!-- card section -->
   <div class="container">
-    <!-- preview -->
     <p class="text-center mt-3">بطاقة التهنئة</p>
     <div class="cardSYO">
       <div class="img">
@@ -102,9 +101,6 @@
     crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIxVXM" crossorigin="anonymous"></script>
-
   <script src="https://unpkg.com/bootstrap-icons@3.1.0"></script>
-
-
 
 </html>
